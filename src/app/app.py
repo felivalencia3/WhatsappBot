ca3248b4da87e21e0ff674a31f1349ac0666ab32
Felipe Valencia <fvalencia@vc-soft.com>
2019-06-26 11:37:17 -0500

    Ready to Upload to Heroku

---
+++ src/app/app.py
@@ -0,0 +1,27 @@
+import requests
+from flask import Flask, render_template, request, make_response
+from twilio.rest import Client
+
+SID = "AC652c51e939f97d17a00c3105769649d7"
+KEY = "df7e3c41ef9a295051542cc50666c3ee"
+
+app = Flask(__name__)
+client = Client(SID, KEY)
+
+
+@app.route('/send', methods=['POST'])
+def incoming():
+    to = request.form['To']
+    origin = request.form['From']
+    response = requests.get('https://quota.glitch.me/random')
+    text = response.json()['quoteText']
+    message = client.messages.create(
+        from_=origin,
+        body=text,
+        to=to
+    )
+    return str(message.error_message or 200)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
